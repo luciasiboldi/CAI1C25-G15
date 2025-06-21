82ae1becf9fc8ca9a68412e59cc388cbcac1b3ea
Ignacio Arzani <ignacioarzani@gmail.com>
2025-06-21 18:10:19 -0300

    Fase 2 - Pantalla de carrito y parte del form.

---
+++ TemplateTPCorto/CarritoForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace TemplateTPCorto
+{
+    partial class CarritoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support – do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelCliente = new System.Windows.Forms.Label();
+            this.cmbClientes = new System.Windows.Forms.ComboBox();
+            this.labelCategoria = new System.Windows.Forms.Label();
+            this.cmbCategorias = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dgvCarrito = new System.Windows.Forms.DataGridView();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarrito)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelCliente
+            // 
+            this.labelCliente.AutoSize = true;
+            this.labelCliente.Location = new System.Drawing.Point(12, 15);
+            this.labelCliente.Name = "labelCliente";
+            this.labelCliente.Size = new System.Drawing.Size(42, 13);
+            this.labelCliente.TabIndex = 0;
+            this.labelCliente.Text = "Cliente";
+            // 
+            // cmbClientes
+            // 
+            this.cmbClientes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbClientes.FormattingEnabled = true;
+            this.cmbClientes.Location = new System.Drawing.Point(70, 12);
+            this.cmbClientes.Name = "cmbClientes";
+            this.cmbClientes.Size = new System.Drawing.Size(200, 21);
+            this.cmbClientes.TabIndex = 1;
+            // 
+            // labelCategoria
+            // 
+            this.labelCategoria.AutoSize = true;
+            this.labelCategoria.Location = new System.Drawing.Point(12, 50);
+            this.labelCategoria.Name = "labelCategoria";
+            this.labelCategoria.Size = new System.Drawing.Size(55, 13);
+            this.labelCategoria.TabIndex = 2;
+            this.labelCategoria.Text = "Categoría";
+            // 
+            // cmbCategorias
+            // 
+            this.cmbCategorias.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategorias.FormattingEnabled = true;
+            this.cmbCategorias.Location = new System.Drawing.Point(70, 47);
+            this.cmbCategorias.Name = "cmbCategorias";
+            this.cmbCategorias.Size = new System.Drawing.Size(200, 21);
+            this.cmbCategorias.TabIndex = 3;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(290, 45);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 23);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.AllowUserToAddRows = false;
+            this.dgvProductos.AllowUserToDeleteRows = false;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(15, 80);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.ReadOnly = true;
+            this.dgvProductos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProductos.Size = new System.Drawing.Size(600, 150);
+            this.dgvProductos.TabIndex = 5;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(630, 150);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(100, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // dgvCarrito
+            // 
+            this.dgvCarrito.AllowUserToAddRows = false;
+            this.dgvCarrito.AllowUserToDeleteRows = false;
+            this.dgvCarrito.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarrito.Location = new System.Drawing.Point(15, 250);
+            this.dgvCarrito.Name = "dgvCarrito";
+            this.dgvCarrito.ReadOnly = true;
+            this.dgvCarrito.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCarrito.Size = new System.Drawing.Size(600, 150);
+            this.dgvCarrito.TabIndex = 7;
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(15, 420);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(49, 13);
+            this.lblSubtotal.TabIndex = 8;
+            this.lblSubtotal.Text = "Subtotal:";
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Location = new System.Drawing.Point(200, 420);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(62, 13);
+            this.lblDescuento.TabIndex = 9;
+            this.lblDescuento.Text = "Descuento:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(400, 415);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(47, 17);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total";
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(630, 415);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(100, 23);
+            this.btnConfirmar.TabIndex = 11;
+            this.btnConfirmar.Text = "Confirmar Venta";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.BtnConfirmar_Click);
+            // 
+            // CarritoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 460);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblDescuento);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.dgvCarrito);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.cmbCategorias);
+            this.Controls.Add(this.labelCategoria);
+            this.Controls.Add(this.cmbClientes);
+            this.Controls.Add(this.labelCliente);
+            this.Name = "CarritoForm";
+            this.Text = "Carrito de Compras";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarrito)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCliente;
+        private System.Windows.Forms.ComboBox cmbClientes;
+        private System.Windows.Forms.Label labelCategoria;
+        private System.Windows.Forms.ComboBox cmbCategorias;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgvCarrito;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnConfirmar;
+    }
+}
---
+++ TemplateTPCorto/CarritoForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemplateTPCorto
+{
+    public partial class CarritoForm : Form
+    {
+        private readonly HttpClient _httpClient;
+        private List<Cliente> _clientes;
+        private List<Producto> _productos;
+        private List<CartItem> _carrito;
+
+        public CarritoForm()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://tuservidor.api/") // TODO: ajustar URL base
+            };
+            _carrito = new List<CartItem>();
+
+            Load += CarritoForm_Load;
+        }
+
+        private async void CarritoForm_Load(object sender, EventArgs e)
+        {
+            await CargarClientesAsync();
+            CargarCategorias();
+            ActualizarCarritoUI();
+        }
+
+        private async Task CargarClientesAsync()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("api/Cliente/GetClientes");
+                response.EnsureSuccessStatusCode();
+                var json = await response.Content.ReadAsStringAsync();
+                _clientes = JsonSerializer.Deserialize<List<Cliente>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                cmbClientes.DataSource = _clientes;
+                cmbClientes.DisplayMember = "Nombre";
+                cmbClientes.ValueMember = "Id";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar clientes: " + ex.Message);
+            }
+        }
+
+        private void CargarCategorias()
+        {
+            var categorias = new List<Categoria>
+            {
+                new Categoria(1, "Audio"),
+                new Categoria(2, "Celulares"),
+                new Categoria(3, "Electro Hogar"),
+                new Categoria(4, "Informática"),
+                new Categoria(5, "Smart TV")
+            };
+            cmbCategorias.DataSource = categorias;
+            cmbCategorias.DisplayMember = "Descripcion";
+            cmbCategorias.ValueMember = "Id";
+        }
+
+        private async void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            if (cmbCategorias.SelectedValue is int idCat)
+            {
+                try
+                {
+                    var response = await _httpClient.GetAsync($"api/Producto/TraerProductosPorCategoria?idCategoria={idCat}");
+                    response.EnsureSuccessStatusCode();
+                    var json = await response.Content.ReadAsStringAsync();
+                    _productos = JsonSerializer.Deserialize<List<Producto>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    dgvProductos.DataSource = _productos;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al obtener productos: " + ex.Message);
+                }
+            }
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            if (dgvProductos.CurrentRow?.DataBoundItem is Producto prod)
+            {
+                var existing = _carrito.FirstOrDefault(c => c.Producto.Id == prod.Id);
+                if (existing != null)
+                    existing.Cantidad++;
+                else
+                    _carrito.Add(new CartItem { Producto = prod, Cantidad = 1 });
+
+                ActualizarCarritoUI();
+            }
+        }
+
+        private async void BtnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (cmbClientes.SelectedValue is int idCliente && _carrito.Any())
+            {
+                var ventaDto = new VentaDto
+                {
+                    ClienteId = idCliente,
+                    Items = _carrito.Select(c => new VentaItemDto { ProductoId = c.Producto.Id, Cantidad = c.Cantidad }).ToList()
+                };
+
+                try
+                {
+                    var payload = JsonSerializer.Serialize(ventaDto);
+                    var content = new StringContent(payload, Encoding.UTF8, "application/json");
+                    var response = await _httpClient.PostAsync("api/Venta/AgregarVenta", content);
+                    response.EnsureSuccessStatusCode();
+
+                    MessageBox.Show("Venta registrada con éxito.");
+                    _carrito.Clear();
+                    ActualizarCarritoUI();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al confirmar venta: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un cliente y agregue al menos un producto.");
+            }
+        }
+
+        private void ActualizarCarritoUI()
+        {
+            dgvCarrito.DataSource = null;
+            dgvCarrito.DataSource = _carrito.Select(c => new
+            {
+                c.Producto.Descripcion,
+                c.Cantidad,
+                Precio = c.Producto.Precio,
+                Subtotal = c.Cantidad * c.Producto.Precio
+            }).ToList();
+
+            decimal subtotal = _carrito.Sum(c => c.Cantidad * c.Producto.Precio);
+            decimal descuento = CalcularDescuento();
+            decimal total = subtotal - descuento;
+
+            lblSubtotal.Text = $"Subtotal: {subtotal:C}";
+            lblDescuento.Text = $"Descuento: {descuento:C}";
+            lblTotal.Text = $"Total: {total:C}";
+        }
+
+        private decimal CalcularDescuento()
+        {
+            var totalElectro = _carrito
+                .Where(c => c.Producto.CategoriaId == 3)
+                .Sum(c => c.Cantidad * c.Producto.Precio);
+
+            if (totalElectro > 1_000_000m)
+                return totalElectro * 0.15m;
+            return 0;
+        }
+    }
+
+    // Modelos auxiliares
+    public class Cliente { public int Id { get; set; } public string Nombre { get; set; } }
+    public class Producto { public int Id { get; set; } public string Descripcion { get; set; } public decimal Precio { get; set; } public int CategoriaId { get; set; } }
+    public class CartItem { public Producto Producto { get; set; } public int Cantidad { get; set; } }
+    public class Categoria { public int Id { get; } public string Descripcion { get; } public Categoria(int id, string desc) { Id = id; Descripcion = desc; } }
+    public class VentaDto { public int ClienteId { get; set; } public List<VentaItemDto> Items { get; set; } }
+    public class VentaItemDto { public int ProductoId { get; set; } public int Cantidad { get; set; } }
+}
